5257d9065c4910d9c544abae4d573882c3161c33
Gustav Bang <gustavbang96@gmail.com>
2018-11-05 21:20:37 +0100

    First commit

---
+++ jobs/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Job
+# Create your views here.
+
+
+def jobs(request):
+    jobs = Job.objects
+    return render(request, "jobs.html", {"jobs": jobs})
+
+
+def detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    return render(request, "detail.html", {"job": job_detail})
